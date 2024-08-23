e5530589a4b396bd8138fbae0583a3efe9271aaf
D_Zucho <danny.nico.dev@outlook.com>
2024-08-22 20:16:11 -0700

    add header. start hero

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 import './header.css'
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <div id='home' className='gpt3__header section__padding'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>
+          Let&apos;s Buit Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+
+        <div className='gpt__header-content__input'>
+          <input type='email' placeholder='Your Email Address' />
+          <buttonm type='button'>Get Started</buttonm>
+        </div>
+
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt='people' />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+
+        <div className='gpt3__header-image'>
+          <img src={ai} alt='AI' />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Header
