df81f17bd602fa89ac2b51be16b8cc7dab31a404
D_Zucho <danny.nico.dev@outlook.com>
2024-08-22 23:53:10 -0700

    add hero section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -19,17 +19,17 @@ const Header = () => {
 
         <div className='gpt__header-content__input'>
           <input type='email' placeholder='Your Email Address' />
-          <buttonm type='button'>Get Started</buttonm>
+          <button type='button'>Get Started</button>
         </div>
 
         <div className='gpt3__header-content__people'>
           <img src={people} alt='people' />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
+      </div>
 
-        <div className='gpt3__header-image'>
-          <img src={ai} alt='AI' />
-        </div>
+      <div className='gpt3__header-image'>
+        <img src={ai} alt='AI' />
       </div>
     </div>
   )
